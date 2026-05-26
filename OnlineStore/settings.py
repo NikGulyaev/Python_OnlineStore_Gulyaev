e18ea84e507df2dabff0ba078538be1ab32cf336
Nikolaj Gulyaev <ns.gulyaev@yandex.ru>
2026-05-27 01:17:51 +0300

    Added psycopg2-binary

--- OnlineStore/settings.py
+++ OnlineStore/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import psycopg2
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'OnlineStore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'onlinestore',
+        'USER': 'gulyaev',
+        'PASSWORD': 'GN68630s',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
